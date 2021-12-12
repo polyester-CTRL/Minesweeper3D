6170e1affc99d9b26dc8b7c1cc55528c68869924
polyester_ctrl <shunfreed2016@gmail.com>
2021-12-13 04:26:45 +0900

    boxの回転を実装

--- Minesweeper3D/Main.cpp
+++ Minesweeper3D/Main.cpp
@@ -1,4 +1,7 @@
 ﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.2
+# include "Array3D.hpp"
+// # include "TorusIntersect.hpp"
+
 
 void Main()
 {
@@ -9,14 +12,42 @@ void Main()
 	const Texture woodTexture{ U"example/texture/wood.jpg", TextureDesc::MippedSRGB };
 	const MSRenderTexture renderTexture{ Scene::Size(), TextureFormat::R8G8B8A8_Unorm_SRGB, HasDepth::Yes };
 	DebugCamera3D camera{ renderTexture.size(), 30_deg, Vec3{ 10, 16, -32 } };
+	FontAsset::Register(U"Number", 10, Typeface::Medium);
 
-	const OrientedBox box{ 0,4,0,4 };
+	const int numOfBlock = 7;
+
+	OrientedBox box{ 0,4,0,1 }, box2{4};
+	
 	Quaternion rotation;
 	bool grabbedX = false;
 	bool grabbedY = false;
 
-	const Cylinder cY{ box.center, 4.6, 1 };
-	const Cylinder cX{ box.center, 4.5, 1, Quaternion::RotateZ(90_deg) };
+
+	const Disc diY{ box.center, 1 };
+	
+
+	const Cylinder cY{ box.center, 6.0, 1 };
+	const Cylinder cX{ box.center, 6.0, 1, Quaternion::RotateZ(90_deg) };
+	const Mesh torusY{ MeshData::Torus(box.center, 6.0, 0.6) };
+	const Mesh torusX{ MeshData::Torus(6.0, 0.6) };
+
+	const Mesh billboard{ MeshData::Billboard() };
+
+	bool isGameover = false;
+	Array<size_t> checked(numOfBlock * numOfBlock * numOfBlock);
+
+	Array3D<std::pair<int32, OrientedBox>> boxes(numOfBlock, numOfBlock, numOfBlock);
+	for (int32 i = 0; i < numOfBlock; i++)
+	{
+		for (int32 j = 0; j < numOfBlock; j++)
+		{
+			for (int32 k = 0; k < numOfBlock; k++)
+			{
+				boxes.set(i, j, k, { (int32)RandomBool(0.2), OrientedBox{ Vec3{i - numOfBlock / 2, j - numOfBlock / 2, k - numOfBlock / 2} * rotation + Vec3{0, 4, 0},
+								 Vec3{0.9, 0.9, 0.9}, rotation} });
+			}
+		}
+	}
 
 	while (System::Update())
 	{
@@ -27,13 +58,79 @@ void Main()
 		{
 			const Ray ray = camera.screenToRay(Cursor::PosF());
 			const ScopedRenderTarget3D target{ renderTexture.clear(backgroundColor) };
+			
 			Plane{ 64 }.draw(uvChecker);
-			box.draw(rotation, woodTexture);
 
-			cY.draw(ColorF{ (grabbedY ? 0.8 : 0.5), 0.0, 0.0 }.removeSRGBCurve());
-			cX.draw(ColorF{ 0.0, (grabbedX ? 0.8 : 0.5), 0.0 }.removeSRGBCurve());
+			// 交差し、もっとも手前にあるボックスのインデックス
+			size_t boxIndex = 998244353;
+			double minDistance = Math::Inf;
 
-			if (grabbedX)
+			for (int i = 0; i < numOfBlock; i++) {
+				for (int j = 0; j < numOfBlock; j++) {
+					for (int k = 0; k < numOfBlock; k++) {
+						auto& tmpbox = boxes.get(i, j, k);
+						tmpbox.second.setPos(Vec3{ i - numOfBlock / 2, j - numOfBlock / 2, k - numOfBlock / 2 } *rotation + Vec3{ 0, 4, 0 });
+						tmpbox.second.setOrientation(rotation);
+						if (tmpbox.first >= 0)
+						{
+							if (const auto distance = ray.intersects(tmpbox.second))
+							{
+								if (*distance < minDistance)
+								{
+									minDistance = *distance;
+									boxIndex = boxes.get_index(i, j, k);
+								}
+							}
+							if(checked[boxes.get_index(i, j, k)])
+							{
+								tmpbox.second.draw(ColorF{ 1.0, 0.8, 0.0, 0.8 });
+							}
+							else
+							{
+								tmpbox.second.draw(ColorF{ 1.0, 1.0, 1.0, 0.8 });
+							}
+							
+							// FontAsset(U"Number")(U"0").draw(camera.billboard(Vec3{i - numOfBlock / 2, j - numOfBlock / 2, k - numOfBlock / 2} *rotation + Vec3{0, 4, 0}, 1));
+						}
+						boxes.set(i, j, k, tmpbox);
+						
+					}
+				}
+			}
+
+			if (minDistance != Math::Inf && boxIndex < 998244353)
+			{
+				auto& tmpbox = boxes.get(boxIndex);
+				if (MouseL.down())
+				{
+					if (tmpbox.first > 0)
+					{
+						// Print << U"Game Over";
+						isGameover = true;
+					}
+					else
+					{
+						boxes.set(boxIndex, { -1, tmpbox.second });
+					}
+				}
+				// 印を付ける
+				else if (MouseR.down())
+				{
+					// 状態を反転
+					checked[boxIndex] ^= 1;
+				}
+			}
+
+			if (isGameover)
+			{
+				Print << U"Game Over";
+			}
+
+
+			// torusY.draw(ColorF{ (grabbedY ? 0.8 : 0.5), 0.0, 0.0 }.removeSRGBCurve());
+			// torusX.draw(box.center, Quaternion::RotateZ(90_deg), ColorF{ 0.0, (grabbedX ? 0.8 : 0.5), 0.0 }.removeSRGBCurve());
+
+			/*if (grabbedX)
 			{
 				rotation *= Quaternion::RotateX(-1_deg * Cursor::DeltaF().y);
 			}
@@ -41,35 +138,53 @@ void Main()
 			if (grabbedY)
 			{
 				rotation *= Quaternion::RotateY(-1_deg * Cursor::DeltaF().x);
-			}
+			}*/
 
-			const Optional<float> cyd = ray.intersects(cY);
-			const Optional<float> cxd = ray.intersects(cX);
-
-			Print << U"cyd: " << cyd;
-			Print << U"cxd: " << cxd;
-
-			if (cyd || cxd)
+			if (KeyA.pressed())
 			{
-				Cursor::RequestStyle(CursorStyle::Hand);
-
-				if (MouseL.down())
-				{
-					if (cxd && cyd)
-					{
-						((cxd < cyd) ? grabbedX : grabbedY) = true;
-					}
-					else
-					{
-						((cxd) ? grabbedX : grabbedY) = true;
-					}
-				}
+				rotation *= Quaternion::RotateY(1_deg);
 			}
-
-			if (MouseL.up())
+			if (KeyD.pressed())
 			{
-				grabbedY = grabbedX = false;
+				rotation *= Quaternion::RotateY(-1_deg);
 			}
+			if (KeyW.pressed())
+			{
+				rotation *= Quaternion::RotateX(1_deg);
+			}
+			if (KeyS.pressed())
+			{
+				rotation *= Quaternion::RotateX(-1_deg);
+			}
+			// const Optional<float> cyd = TorusXYIntersect(ray, { box.center, 6.0}, box.center);
+			//const Optional<float> cyd = ray.intersects(cY);
+			//const Optional<float> cxd = ray.intersects(cX);
+			//// torusX.boundingSphere().draw();
+
+			//Print << U"cyd: " << cyd;
+			//Print << U"cxd: " << cxd;
+
+			//if (cyd || cxd)
+			//{
+			//	Cursor::RequestStyle(CursorStyle::Hand);
+
+			//	if (MouseL.down())
+			//	{
+			//		if (cxd && cyd)
+			//		{
+			//			((cxd < cyd) ? grabbedX : grabbedY) = true;
+			//		}
+			//		else
+			//		{
+			//			((cxd) ? grabbedX : grabbedY) = true;
+			//		}
+			//	}
+			//}
+
+			//if (MouseL.up())
+			//{
+			//	grabbedY = grabbedX = false;
+			//}
 		}
 
 		{
---
+++ Minesweeper3D/TorusIntersect.hpp
@@ -0,0 +1,156 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.2
+double TorusIntersect_implXZ(const Ray& ray, const Vec2& tor)
+{
+	const Vec3 ro = ray.origin.xyz();
+	const Vec3 rd = ray.direction.xyz();
+
+	double po = 1.0;
+	double Ra2 = tor.x * tor.x;
+	double ra2 = tor.y * tor.y;
+	double m = ro.dot(ro);
+	double n = ro.dot(rd);
+	double k = (m + Ra2 - ra2) / 2.0;
+	double k3 = n;
+	double k2 = n * n - Ra2 * rd.xz().dot(rd.xz()) + k;
+	double k1 = n * k - Ra2 * rd.xz().dot(ro.xz());
+	double k0 = k * k - Ra2 * ro.xz().dot(ro.xz());
+
+	if (std::abs(k3 * (k3 * k3 - k2) + k1) < 0.01)
+	{
+		po = -1.0;
+		double tmp = k1; k1 = k3; k3 = tmp;
+		k0 = 1.0 / k0;
+		k1 = k1 * k0;
+		k2 = k2 * k0;
+		k3 = k3 * k0;
+	}
+
+	double c2 = k2 * 2.0 - 3.0 * k3 * k3;
+	double c1 = k3 * (k3 * k3 - k2) + k1;
+	double c0 = k3 * (k3 * (c2 + 2.0 * k2) - 8.0 * k1) + 4.0 * k0;
+	c2 /= 3.0;
+	c1 *= 2.0;
+	c0 /= 3.0;
+	double Q = c2 * c2 + c0;
+	double R = c2 * c2 * c2 - 3.0 * c2 * c0 + c1 * c1;
+	double h = R * R - Q * Q * Q;
+
+	if (h >= 0.0)
+	{
+		h = sqrt(h);
+		double v = Math::Sign(R + h) * pow(abs(R + h), 1.0 / 3.0); // cube root
+		double u = Math::Sign(R - h) * pow(abs(R - h), 1.0 / 3.0); // cube root
+		const Vec2 s = Vec2((v + u) + 4.0 * c2, (v - u) * std::sqrt(3.0));
+		double y = sqrt(0.5 * (s.length() + s.x));
+		double x = 0.5 * s.y / y;
+		double r = 2.0 * c1 / (x * x + y * y);
+		double t1 = x - r - k3; t1 = (po < 0.0) ? 2.0 / t1 : t1;
+		double t2 = -x - r - k3; t2 = (po < 0.0) ? 2.0 / t2 : t2;
+		double t = 1e20;
+		if (t1 > 0.0) t = t1;
+		if (t2 > 0.0) t = Min(t, t2);
+		return t;
+	}
+
+	double sQ = sqrt(Q);
+	double w = sQ * cos(acos(-R / (sQ * Q)) / 3.0);
+	double d2 = -(w + c2); if (d2 < 0.0) return -1.0;
+	double d1 = sqrt(d2);
+	double h1 = sqrt(w - 2.0 * c2 + c1 / d1);
+	double h2 = sqrt(w - 2.0 * c2 - c1 / d1);
+	double t1 = -d1 - h1 - k3; t1 = (po < 0.0) ? 2.0 / t1 : t1;
+	double t2 = -d1 + h1 - k3; t2 = (po < 0.0) ? 2.0 / t2 : t2;
+	double t3 = d1 - h2 - k3; t3 = (po < 0.0) ? 2.0 / t3 : t3;
+	double t4 = d1 + h2 - k3; t4 = (po < 0.0) ? 2.0 / t4 : t4;
+	double t = 1e20;
+	if (t1 > 0.0) t = t1;
+	if (t2 > 0.0) t = Min(t, t2);
+	if (t3 > 0.0) t = Min(t, t3);
+	if (t4 > 0.0) t = Min(t, t4);
+	return t;
+}
+
+double TorusIntersect_implXY(const Ray& ray, const Vec2& tor)
+{
+	const Vec3 ro = ray.origin.xyz();
+	const Vec3 rd = ray.direction.xyz();
+
+	double po = 1.0;
+	double Ra2 = tor.x * tor.x;
+	double ra2 = tor.y * tor.y;
+	double m = ro.dot(ro);
+	double n = ro.dot(rd);
+	double k = (m + Ra2 - ra2) / 2.0;
+	double k3 = n;
+	double k2 = n * n - Ra2 * rd.xy().dot(rd.xy()) + k;
+	double k1 = n * k - Ra2 * rd.xy().dot(ro.xy());
+	double k0 = k * k - Ra2 * ro.xy().dot(ro.xy());
+
+	if (std::abs(k3 * (k3 * k3 - k2) + k1) < 0.01)
+	{
+		po = -1.0;
+		double tmp = k1; k1 = k3; k3 = tmp;
+		k0 = 1.0 / k0;
+		k1 = k1 * k0;
+		k2 = k2 * k0;
+		k3 = k3 * k0;
+	}
+
+	double c2 = k2 * 2.0 - 3.0 * k3 * k3;
+	double c1 = k3 * (k3 * k3 - k2) + k1;
+	double c0 = k3 * (k3 * (c2 + 2.0 * k2) - 8.0 * k1) + 4.0 * k0;
+	c2 /= 3.0;
+	c1 *= 2.0;
+	c0 /= 3.0;
+	double Q = c2 * c2 + c0;
+	double R = c2 * c2 * c2 - 3.0 * c2 * c0 + c1 * c1;
+	double h = R * R - Q * Q * Q;
+
+	if (h >= 0.0)
+	{
+		h = sqrt(h);
+		double v = Math::Sign(R + h) * pow(abs(R + h), 1.0 / 3.0); // cube root
+		double u = Math::Sign(R - h) * pow(abs(R - h), 1.0 / 3.0); // cube root
+		const Vec2 s = Vec2((v + u) + 4.0 * c2, (v - u) * std::sqrt(3.0));
+		double y = sqrt(0.5 * (s.length() + s.x));
+		double x = 0.5 * s.y / y;
+		double r = 2.0 * c1 / (x * x + y * y);
+		double t1 = x - r - k3; t1 = (po < 0.0) ? 2.0 / t1 : t1;
+		double t2 = -x - r - k3; t2 = (po < 0.0) ? 2.0 / t2 : t2;
+		double t = 1e20;
+		if (t1 > 0.0) t = t1;
+		if (t2 > 0.0) t = Min(t, t2);
+		return t;
+	}
+
+	double sQ = sqrt(Q);
+	double w = sQ * cos(acos(-R / (sQ * Q)) / 3.0);
+	double d2 = -(w + c2); if (d2 < 0.0) return -1.0;
+	double d1 = sqrt(d2);
+	double h1 = sqrt(w - 2.0 * c2 + c1 / d1);
+	double h2 = sqrt(w - 2.0 * c2 - c1 / d1);
+	double t1 = -d1 - h1 - k3; t1 = (po < 0.0) ? 2.0 / t1 : t1;
+	double t2 = -d1 + h1 - k3; t2 = (po < 0.0) ? 2.0 / t2 : t2;
+	double t3 = d1 - h2 - k3; t3 = (po < 0.0) ? 2.0 / t3 : t3;
+	double t4 = d1 + h2 - k3; t4 = (po < 0.0) ? 2.0 / t4 : t4;
+	double t = 1e20;
+	if (t1 > 0.0) t = t1;
+	if (t2 > 0.0) t = Min(t, t2);
+	if (t3 > 0.0) t = Min(t, t3);
+	if (t4 > 0.0) t = Min(t, t4);
+	return t;
+}
+
+bool TorusXYIntersect(const Ray& ray, const Vec2& tor, const Vec3& torusPos)
+{
+	Ray r = ray;
+	r.origin = SIMD_double4{ r.origin.xyz() - torusPos, 0.0f };
+	return (0.0 < TorusIntersect_implXY(r, tor));
+}
+
+bool TorusXZIntersect(const Ray& ray, const Vec2& tor, const Vec3& torusPos)
+{
+	Ray r = ray;
+	r.origin = SIMD_double4{ r.origin.xyz() - torusPos, 0.0f };
+	return (0.0 < TorusIntersect_implXZ(r, tor));
+}
